9019e7ad0d3526885114a00e582310d66d85c031
Eric Hester <ehes5653@uni.sydney.edu.au>
2020-06-18 22:24:56 +1000

    salty-boussinesq: ipynb of diagram plots, example simulation code, and analysis and plotting of simulations.

---
+++ run-sequence.bash
@@ -0,0 +1,4 @@
+source ~/.bash_profile
+#python3 flow-stationary-poiseuille-vp-tangent.py 1e3
+python3 flow-stationary-poiseuille-vp-tangent.py 1e4
+python3 flow-stationary-poiseuille-vp-tangent.py 1e5
