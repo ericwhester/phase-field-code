f589e8f8447bb3c46c578a3003ae241ca6d6f944
Eric Hester <ehes5653@uni.sydney.edu.au>
2020-06-18 22:26:56 +1000

    remove accidentally added code.

--- run-sequence.bash
+++
@@ -1,4 +0,0 @@
-source ~/.bash_profile
-#python3 flow-stationary-poiseuille-vp-tangent.py 1e3
-python3 flow-stationary-poiseuille-vp-tangent.py 1e4
-python3 flow-stationary-poiseuille-vp-tangent.py 1e5
